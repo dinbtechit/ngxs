d6b19ba46df67465c1ef6e0c6d558eb9d0c07a9b
Dinesh Srini <dinbtechit@gmail.com>
2023-08-25 19:05:19 -0300

    Disable testing code

--- src/main/kotlin/com/github/dinbtechit/ngxs/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dinbtechit/ngxs/services/MyProjectService.kt
@@ -1,17 +1,14 @@
 package com.github.dinbtechit.ngxs.services
 
+import com.github.dinbtechit.ngxs.NgxsBundle
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.github.dinbtechit.ngxs.NgxsBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
 
     init {
         thisLogger().info(NgxsBundle.message("projectService", project.name))
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
-
-    fun getRandomNumber() = (1..100).random()
 }
