a08f3ea93b3f855b857b2869c502248d564c82e7
Dinesh <dinbtechit@gmail.com>
2023-08-26 12:34:18 -0300

    Revert "Feature/initial"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,4 @@
 
 ## [Unreleased]
 ### Added
-- Initial NGXS Store Generation
-   
 - Initial scaffold created from [IntelliJ Platform Plugin Template](https://github.com/JetBrains/intellij-platform-plugin-template)
--- src/test/kotlin/com/github/dinbtechit/ngxs/MyPluginTest.kt
+++ src/test/kotlin/com/github/dinbtechit/ngxs/MyPluginTest.kt
@@ -1,13 +1,12 @@
 package com.github.dinbtechit.ngxs
 
-import com.github.dinbtechit.ngxs.action.cli.store.CLIState
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import junit.framework.TestCase
+import com.github.dinbtechit.ngxs.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
@@ -31,8 +30,9 @@ class MyPluginTest : BasePlatformTestCase() {
     }
 
     fun testProjectService() {
-        val projectService = project.service<CLIState>()
-        TestCase.assertNotNull(projectService)
+        val projectService = project.service<MyProjectService>()
+
+        assertNotSame(projectService.getRandomNumber(), projectService.getRandomNumber())
     }
 
     override fun getTestDataPath() = "src/test/testData/rename"
